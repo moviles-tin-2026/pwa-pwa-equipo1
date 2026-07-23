bd8c234266b95b76770219364c174a21cad5df55
facu100201 <183398552+facu100201@users.noreply.github.com>
2026-07-23 16:28:36 +0000

    build: actualizar GitHub Pages con últimos cambios [automatizado]

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "37121987" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2095923753" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
