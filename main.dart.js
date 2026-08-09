ff456260b8d54f7a339601b7b03885c915a60f6e
facu100201 <183398552+facu100201@users.noreply.github.com>
2026-08-09 20:20:17 +0000

    build: actualizar GitHub Pages con últimos cambios [automatizado]

--- main.dart.js
+++ main.dart.js
@@ -33897,7 +33897,7 @@ $S:2}
 A.aNZ.prototype={
 $1(a){var s=A.dY().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/0cd610717bde95fd88343c64f81c11ba4e5c0010/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/5a2a6a42cce67f965cf540fcecf616faca624aa1/":s)+a},
 $S:69}
 A.Ts.prototype={
 gA(a){var s=this.a
